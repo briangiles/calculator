2661964b6d46ebc0b963ff0bbec163bb3006bcc6
Brian Giles <briangiles@gmail.com>
2016-12-11 19:37:16 -0800

    added subtraction, multiply, and divide functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,30 @@
+//add
 var add = function(number1, number2) {
 	return number1 + number2;
 };
 
+//parseInt converts users inputed text from a String and converts it to a number
 var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
-alert(add(number1, number2));
+
+//subtract
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+//multiply
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+//divide
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+//Result (add, subtract, multiply, divide)
+//var result = add(number1, number2); //add
+//var result = subtract(number1, number2);
+//var result = multiply(number1, number2);
+var result = divide(number1, number2);
+alert(result);
